6a1b7e9538788ea96944467e5daf17c9072f9247
Louis Mellac <louismellac@gmail.com>
2023-05-29 17:58:57 +0100

    Can see a user's bookings and create new bookings, but this doesn't work well because we don't log in so current_user is always nil

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,35 @@
 class BookingsController < ApplicationController
+  def index
+    current_user = User.find(1)
+
+    if !current_user.nil?
+      @bookings = Booking.where(user_id: current_user.id)
+    else
+      @bookings = nil
+    end
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    booking = Booking.new(booking_params)
+    friend = Friend.find(params[:friend_id])
+
+    booking.friend = friend
+    booking.user = current_user
+
+    if booking.save!
+      redirect_to bookings_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "friends#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :bookings, only: [:new, :create, :index]
 end
