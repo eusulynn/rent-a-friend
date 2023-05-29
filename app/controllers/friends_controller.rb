31a54e64d82a64ad048ea1f688723a602c77b6bc
Su Lynn Eu <eusulynn@gmail.com>
2023-05-29 17:49:32 +0100

    Added #create and #new for friends controller, added a new form

--- app/controllers/friends_controller.rb
+++ app/controllers/friends_controller.rb
@@ -1,2 +1,20 @@
 class FriendsController < ApplicationController
+  def new
+    @friend = Friend.new
+  end
+
+  def create
+    @friend = Friend.new(friend_params)
+    @friend.user = current_user
+    if @friend.save
+      redirect_to friend_path(@friend)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def friend_params
+    params.require(:friend).permit(:location, :price, :name, :age, :gender, :language)
+  end
 end
