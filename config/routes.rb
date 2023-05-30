f842e5281586ca4f4ed82be273bd91064c089f6e
Louis Mellac <louismellac@gmail.com>
2023-05-30 14:27:53 +0100

    added a button on bookings page to cancel a booking

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "friends#index"
   resources :friends, only: [:index, :show, :new, :create, :destroy]
-  resources :bookings, only: [:new, :create, :index]
+  resources :bookings, only: [:new, :create, :index, :destroy]
 end
